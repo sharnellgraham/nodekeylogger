014d1edb04c47eaf36403a60118d5e61f3a2e0a6
Sharnell Graham <sharnellggraham@gmail.com>
2023-12-06 17:09:28 -0500

    Implement event handling for keyboard data

--- src/index.js
+++ src/index.js
@@ -6,3 +6,21 @@ const toKey = require('./keycodes');
 const EVENT_TYPES = ['keyup', 'keypress', 'keydown'];
 const EV_KEY = 1;
 
+function Keyboard(dev) {
+    this.dev = dev || 'event0';
+    this.bufferSize = 24;
+    this.buffer = new Buffer(this.bufferSize);
+    this.data = fs.createReadStream(`/dev/input/${this.dev}`);
+    this.onRead();
+  }
+  Keyboard.prototype.onRead = function onRead() {
+    const self = this;
+  
+    this.data.on('data', data => {
+      this.buffer = data.slice(24);
+      let event = parse(this, this.buffer);
+      if (event) {
+        event.dev = self.dev;
+        self.emit(event.type, event);
+      }
+    });
